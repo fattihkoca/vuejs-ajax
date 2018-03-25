1f227f1df6836c9444a8e0a557d4073c1dee0a45
fattihkoca <fattih@fattih.com>
2018-03-25 23:53:27 +0300

    Added Laravel route web.php

--- examples/web.php
+++ examples/web.php
@@ -17,16 +17,16 @@ Route::group(['prefix' => 'vue-ajax'], function () {
         $data['title'] = 'GET response from server';
         $data['subtitle'] = isset($_GET['relParameter']) ? $_GET['relParameter'] : 'Completed';
         $data['time'] = date("d.m.Y H:i:s");
-    
+
         return response(json_encode($data), 200)->header('Content-Type', 'application/json; charset=utf-8');
     });
-    
+
     Route::post('file', function () {
         $images = [];
 
-        if(isset($_FILES['images'])) {
-            
-            if(is_array($_FILES['images']['name']) && count($_FILES['images']['name']) > 1) {
+        if (isset($_FILES['images'])) {
+
+            if (is_array($_FILES['images']['name']) && count($_FILES['images']['name']) > 1) {
                 foreach ($_FILES['images']['name'] as $key => $value) {
                     $fileName = $_FILES['images']['name'][$key];
                     $fileType = $_FILES['images']['type'][$key];
@@ -40,9 +40,7 @@ Route::group(['prefix' => 'vue-ajax'], function () {
                 $images[] = [$fileName, 'data:' . $fileType . ';base64,' . base64_encode($fileContent), $fileType];
             }
         }
-    
-        $file_count = is_array($images) ? count($images) : '';
-    
+
         $data = isset($_POST) ? $_POST : [];
         $data['title'] = 'File upload response from server';
         $data['subtitle'] = isset($_POST['relParameter']) ? $_POST['relParameter'] : 'Completed';
@@ -51,14 +49,14 @@ Route::group(['prefix' => 'vue-ajax'], function () {
 
         return response(json_encode($data), 200)->header('Content-Type', 'application/json; charset=utf-8');
     });
-    
+
     Route::get('jsonp', function () {
         $data = isset($_GET) ? $_GET : [];
         $data['title'] = 'JSONP response from server';
         $data['subtitle'] = isset($_GET['relParameter']) ? $_GET['relParameter'] : 'Completed';
         $data['time'] = date("d.m.Y H:i:s");
         $data['callback'] = isset($data['callback']) ? $data['callback'] : 'callback';
-    
+
         return response($data['callback'] . '(' . json_encode($data) . ')', 200)->header('Content-Type', 'text/javascript; charset=utf-8');
     });
 });
